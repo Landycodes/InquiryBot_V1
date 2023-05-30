567d56840b129b3a9bf893325e485d393d7405b8
landycodes <landryandrewsk8@gmail.com>
2023-05-29 17:45:50 -0700

    wrapped request in a promise & try/catch

--- handler/response/callSendAPI.js
+++ handler/response/callSendAPI.js
@@ -5,31 +5,38 @@ function callSendAPI(senderPsid, response) {
   // The page access token we have generated in your app settings
   const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
-  // Construct the message body
-  let requestBody = {
-    recipient: {
-      id: senderPsid,
-    },
-    message: response,
-  };
-  console.log(requestBody);
-  // Send the HTTP request to the Messenger Platform
-  request(
-    {
-      uri: "https://graph.facebook.com/v17.0/me/messages",
-      qs: { access_token: PAGE_ACCESS_TOKEN },
-      method: "POST",
-      json: requestBody,
-    },
-    (err, _res, _body) => {
-      if (!err) {
-        console.log("Message sent!");
-        console.log(_body);
-      } else {
-        console.error("Unable to send message:" + err);
-      }
+  return new Promise((resolve, reject) => {
+    try {
+      // Construct the message body
+      let requestBody = {
+        recipient: {
+          id: senderPsid,
+        },
+        message: response,
+      };
+      console.log(requestBody);
+
+      // Send the HTTP request to the Messenger Platform
+      request(
+        {
+          uri: "https://graph.facebook.com/v17.0/me/messages",
+          qs: { access_token: PAGE_ACCESS_TOKEN },
+          method: "POST",
+          json: requestBody,
+        },
+        (err, _res, _body) => {
+          if (!err) {
+            resolve("Message sent!");
+            console.log(_body);
+          } else {
+            reject("Unable to send message:" + err);
+          }
+        }
+      );
+    } catch (err) {
+      reject(err);
     }
-  );
+  });
 }
 
 module.exports = callSendAPI;
