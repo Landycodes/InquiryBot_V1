af775c7907d7bd8009e30f8593fa32f2f1b1facd
landycodes <landryandrewsk8@gmail.com>
2023-06-30 15:17:46 -0700

    fixed callSendApi uri string

--- handler/response/callSendAPI.js
+++ handler/response/callSendAPI.js
@@ -20,7 +20,7 @@ function callSendAPI(senderPsid, response) {
       // Send the HTTP request to the Messenger Platform
       request(
         {
-          uri: "https://graph.facebook.com/v16.0/me/messages",
+          uri: "https://graph.facebook.com/v6.0/me/messages",
           qs: { access_token: PAGE_ACCESS_TOKEN },
           method: "POST",
           json: requestBody,
