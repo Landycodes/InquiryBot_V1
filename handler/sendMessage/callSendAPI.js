eed1ae0a8948d82a494d0c0439f131de726dc58c
landycodes <landryandrewsk8@gmail.com>
2023-07-06 20:31:02 -0700

    await axios in promise

--- handler/sendMessage/callSendAPI.js
+++ handler/sendMessage/callSendAPI.js
@@ -4,81 +4,72 @@ const axios = require("axios");
 require("dotenv").config({ path: "../../.env" });
 
 // Sends response messages via the Send API
-async function callSendAPI(senderPsid, response) {
+function callSendAPI(senderPsid, response) {
   // The page access token we have generated in your app settings
   const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
-  // return new Promise((resolve, reject) => {
-  try {
-    // Construct the message body
-    let requestBody = {
-      recipient: {
-        id: senderPsid,
-      },
-      message: response,
-    };
-    console.log("-------callSendAPI Request body-------");
-    console.log(requestBody);
+  return new Promise(async (resolve, reject) => {
+    try {
+      // Construct the message body
+      let requestBody = {
+        recipient: {
+          id: senderPsid,
+        },
+        message: response,
+      };
+      console.log("-------callSendAPI Request body-------");
+      console.log(requestBody);
 
-    // Send the HTTP request to the Messenger Platform
-    console.log("sending request... 🤓");
+      // Send the HTTP request to the Messenger Platform
+      console.log("sending request... 🤓");
 
-    const apiResponse = await axios.post(
-      "https://graph.facebook.com/v16.0/me/messages",
-      requestBody,
-      {
-        params: { access_token: PAGE_ACCESS_TOKEN },
-      }
-    );
-    // .then((response) => {
-    //   console.log("request sent... 🙏");
-    //   console.log("Success!!! 🎉");
-    //   resolve(response);
-    // })
-    // .catch((error) => {
-    //   console.log("something fucked up 🤬");
-    //   console.log(error.response.data);
-    //   console.log(requestBody.message);
-    //   console.log("😡 🤬 😤 😖");
-    //   reject("Unable to send message: " + error);
-    // });
+      await axios
+        .post("https://graph.facebook.com/v16.0/me/messages", requestBody, {
+          params: { access_token: PAGE_ACCESS_TOKEN },
+        })
+        .then((response) => {
+          console.log("request sent... 🙏");
+          console.log("Success!!! 🎉");
+          resolve(response.statusText);
+        })
+        .catch((error) => {
+          console.log("something fucked up 🤬");
+          console.log(error.response.data);
+          console.log(requestBody.message);
+          console.log("😡 🤬 😤 😖");
+          reject("Unable to send message: " + error);
+        });
+      return;
 
-    // request(
-    //   {
-    //     uri: "https://graph.facebook.com/v16.0/me/messages",
-    //     qs: { access_token: PAGE_ACCESS_TOKEN },
-    //     method: "POST",
-    //     json: requestBody,
-    //   },
-    //   (err, _res, _body) => {
-    //     console.log("request sent... 🙏");
-    //     if (!err) {
-    //       console.log("Success!!! 🎉");
-    //       resolve(requestBody.message);
-    //     } else if (_body === undefined) {
-    //       console.log("Didnt get a response object 😕");
-    //       reject(err);
-    //     } else {
-    //       console.log("something fucked up 🤬");
-    //       console.log(_body);
-    //       console.log(requestBody.message);
-    //       console.log("😡 🤬 😤 😖");
-    //       reject("Unable to send message: " + err);
-    //     }
-    //   }
-    // );
-    console.log("request sent... 🙏");
-    console.log("Success!!! 🎉");
-    return apiResponse;
-  } catch (err) {
-    // reject(err);
-    console.log("something fucked up 🤬");
-    console.log(error.response.data);
-    console.log(requestBody.message);
-    console.log("😡 🤬 😤 😖");
-    throw new Error("Unable to send message: " + err);
-  }
-  // });
+      // request(
+      //   {
+      //     uri: "https://graph.facebook.com/v16.0/me/messages",
+      //     qs: { access_token: PAGE_ACCESS_TOKEN },
+      //     method: "POST",
+      //     json: requestBody,
+      //   },
+      //   (err, _res, _body) => {
+      //     console.log("request sent... 🙏");
+      //     if (!err) {
+      //       console.log("Success!!! 🎉");
+      //       resolve(requestBody.message);
+      //     } else if (_body === undefined) {
+      //       console.log("Didnt get a response object 😕");
+      //       reject(err);
+      //     } else {
+      //       console.log("something fucked up 🤬");
+      //       console.log(_body);
+      //       console.log(requestBody.message);
+      //       console.log("😡 🤬 😤 😖");
+      //       reject("Unable to send message: " + err);
+      //     }
+      //   }
+      // );
+    } catch (err) {
+      console.log(err);
+      reject(err);
+    }
+  });
 }
 
 // callSendAPI("6143614182425714", {
