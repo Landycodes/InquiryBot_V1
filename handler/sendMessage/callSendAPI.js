710b6489304cd032b029b779e03d4a8785c1e3bb
landycodes <landryandrewsk8@gmail.com>
2023-07-06 18:24:30 -0700

    refactored request to axios

--- handler/sendMessage/callSendAPI.js
+++ handler/sendMessage/callSendAPI.js
@@ -1,4 +1,6 @@
 const request = require("request");
+const axios = require("axios");
+
 require("dotenv").config({ path: "../../.env" });
 
 // Sends response messages via the Send API
@@ -21,30 +23,47 @@ function callSendAPI(senderPsid, response) {
       // Send the HTTP request to the Messenger Platform
       console.log("sending request... 🤓");
 
-      request(
-        {
-          uri: "https://graph.facebook.com/v16.0/me/messages",
-          qs: { access_token: PAGE_ACCESS_TOKEN },
-          method: "POST",
-          json: requestBody,
-        },
-        (err, _res, _body) => {
+      axios
+        .post("https://graph.facebook.com/v16.0/me/messages", requestBody, {
+          params: { access_token: PAGE_ACCESS_TOKEN },
+        })
+        .then((response) => {
           console.log("request sent... 🙏");
-          if (!err) {
-            console.log("Success!!! 🎉");
-            resolve(requestBody.message);
-          } else if (_body === undefined) {
-            console.log("Didnt get a response object 😕");
-            reject(err);
-          } else {
-            console.log("something fucked up 🤬");
-            console.log(_body);
-            console.log(requestBody.message);
-            console.log("😡 🤬 😤 😖");
-            reject("Unable to send message: " + err);
-          }
-        }
-      );
+          console.log("Success!!! 🎉");
+          resolve(response);
+        })
+        .catch((error) => {
+          console.log("something fucked up 🤬");
+          console.log(error.response.data);
+          console.log(requestBody.message);
+          console.log("😡 🤬 😤 😖");
+          reject("Unable to send message: " + error);
+        });
+
+      // request(
+      //   {
+      //     uri: "https://graph.facebook.com/v16.0/me/messages",
+      //     qs: { access_token: PAGE_ACCESS_TOKEN },
+      //     method: "POST",
+      //     json: requestBody,
+      //   },
+      //   (err, _res, _body) => {
+      //     console.log("request sent... 🙏");
+      //     if (!err) {
+      //       console.log("Success!!! 🎉");
+      //       resolve(requestBody.message);
+      //     } else if (_body === undefined) {
+      //       console.log("Didnt get a response object 😕");
+      //       reject(err);
+      //     } else {
+      //       console.log("something fucked up 🤬");
+      //       console.log(_body);
+      //       console.log(requestBody.message);
+      //       console.log("😡 🤬 😤 😖");
+      //       reject("Unable to send message: " + err);
+      //     }
+      //   }
+      // );
     } catch (err) {
       reject(err);
     }
